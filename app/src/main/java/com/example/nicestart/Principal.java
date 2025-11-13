0dddd9139cd844033b0b6436e1e0b4292063ad16
Ainoha <ainohayt06@gmail.com>
2025-11-13 13:53:57 +0100

    Cambios

--- app/src/main/java/com/example/nicestart/Principal.java
+++ app/src/main/java/com/example/nicestart/Principal.java
@@ -20,24 +20,21 @@ public class Principal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.principal);
-
-        // Asegúrate de que este ID existe en tu layout principal.xml
         ImageView background = findViewById(R.id.backView);
         ImageView logo = findViewById(R.id.logosplash);
 
         Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
         logo.startAnimation(fadeIn);
 
-        // Cargar imagen con Glide en el background
         Glide.with(this)
                 .load("https://images.unsplash.com/photo-1565214975484-3cfa9e56f914?ixlib=rb-1.2.1&auto=format&fit=crop&w=1482&q=80")
                 .transition(DrawableTransitionOptions.withCrossFade(100))
                 .centerCrop()
                 .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .placeholder(new ColorDrawable(this.getResources().getColor(R.color.teal_200)))
-                .into(background); // ¡IMPORTANTE! Asignar a un ImageView
+                .into(background);
 
-        openApp(); // Llamar al método que abre la siguiente actividad
+        openApp();
     }
 
     private void openApp() {
